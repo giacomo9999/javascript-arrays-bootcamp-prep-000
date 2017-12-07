2cc4225eeeca32c49316f86a176315704a01b01c
James Gary <bucket@earthlink.net>
2017-12-07 20:40:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// describe('chocolateBars', () => {
+//   it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"',
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element)
+{
+  return [element,...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element)
+{
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element)
+{
+  return [...array,element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  return array;
+}
